a7e8af35e34ee45217f29e3b7c6d19d8cca2fe83
Alvaro Saavedra <saavedra.calero.alvaro@gmail.com>
2022-11-22 17:21:27 +0100

    Clase User añadido

---
+++ app/src/main/java/models/User.java
@@ -0,0 +1,63 @@
+package models;
+
+import java.util.Objects;
+
+public class User {
+
+    // propiedades de clase
+    private String username;
+    private String password;
+
+    // constructores
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public User() {}
+
+    // getter y setter
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // equals, hashcode y toString
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getUsername(), user.getUsername()) && Objects.equals(getPassword(), user.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername(), getPassword());
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
