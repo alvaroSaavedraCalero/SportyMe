1381d3fc2dc99b03cc0451bdd272528ef57517d0
palermou <victor.sanchezg.alcala@gmail.com>
2022-11-23 18:29:43 +0100

    ya deberian estar las clases usuarios y direcciones con los atributos bien

--- app/src/main/java/models/User.java
+++ app/src/main/java/models/User.java
@@ -7,14 +7,24 @@ public class User {
     // propiedades de clase
     private String username;
     private String password;
+    private String nombre;
+    private String apellidos;
+    private String correo;
     private Pedido pedidoActual;
     private Direccion direccionEnvio;
 
     // constructores
-    public User(String username, String password, Pedido pedidoActual) {
+
+
+    public User(String username, String password, String nombre, String apellidos, String correo, Pedido pedidoActual, Direccion direccionEnvio) {
         this.username = username;
         this.password = password;
-        this.pedidoActual=new Pedido();
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.pedidoActual = new Pedido();
+        this.direccionEnvio = new Direccion(direccionEnvio.getCalle(), direccionEnvio.getCP(), direccionEnvio.getProvincia(),
+                direccionEnvio.getLocalidad());
     }
 
     public User(String username) {
@@ -40,9 +50,27 @@ public class User {
         this.password = password;
     }
 
-    public Pedido getPedidoActual(){return pedidoActual;}
+    public Pedido getPedidoActual(){ return pedidoActual;}
 
     public void setPedidoActual(Pedido pedidoActual){this.pedidoActual=pedidoActual;}
+
+    public String getNombre() { return nombre; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getApellidos() { return apellidos; }
+
+    public void setApellidos(String apellidos) { this.apellidos = apellidos; }
+
+    public String getCorreo() { return correo; }
+
+    public void setCorreo(String correo) { this.correo = correo; }
+
+    public Direccion getDireccionEnvio() { return direccionEnvio; }
+
+    public void setDireccionEnvio(Direccion direccionEnvio) { this.direccionEnvio =
+            new Direccion(direccionEnvio.getCalle(), direccionEnvio.getCP(), direccionEnvio.getProvincia(),
+                    direccionEnvio.getLocalidad()); }
 
     // equals, hashcode y toString
 
