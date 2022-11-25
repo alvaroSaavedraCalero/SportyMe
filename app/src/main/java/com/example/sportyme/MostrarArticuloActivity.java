646ea25c44a9b59684157809dab6859e76da4629
Alvaro Saavedra <saavedra.calero.alvaro@gmail.com>
2022-11-25 19:18:48 +0100

    Cambiando el activity_principal.xml para mostar el nombre de cada producto

--- app/src/main/java/com/example/sportyme/MostrarArticuloActivity.java
+++ app/src/main/java/com/example/sportyme/MostrarArticuloActivity.java
@@ -2,13 +2,21 @@ package com.example.sportyme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MostrarArticuloActivity extends AppCompatActivity {
+
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_articulo);
+
+        intent = getIntent();
+
+        int idPrenda = intent.getIntExtra("idImagen", 0);
+
     }
 }
