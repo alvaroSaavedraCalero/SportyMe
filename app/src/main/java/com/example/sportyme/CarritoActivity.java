4bd5a1b31980dc250e70bf4c71bc25fdc2491be4
alvaroSaavedraCalero <91503108+alvaroSaavedraCalero@users.noreply.github.com>
2022-11-26 14:06:29 +0100

    Vista para carrito añadida, falta modificar

--- app/src/main/java/com/example/sportyme/CarritoActivity.java
+++ app/src/main/java/com/example/sportyme/CarritoActivity.java
@@ -10,5 +10,7 @@ public class CarritoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrito);
+
+        String s = "cambios";
     }
 }
