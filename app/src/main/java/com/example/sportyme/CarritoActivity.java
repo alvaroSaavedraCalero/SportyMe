d85739656157ec5546e3f261878fd179ba30a65f
Alvaro Saavedra <saavedra.calero.alvaro@gmail.com>
2022-11-29 17:27:47 +0100

    el problema tiene que estar en el onlclik de añadir al carrito

--- app/src/main/java/com/example/sportyme/CarritoActivity.java
+++ app/src/main/java/com/example/sportyme/CarritoActivity.java
@@ -1,17 +1,22 @@
 package com.example.sportyme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Arrays;
 
 import models.Almacen;
 import models.ItemPedido;
@@ -28,6 +33,7 @@ public class CarritoActivity extends AppCompatActivity {
     private TextView subtotal;
     private TextView gastosEnvio;
     private TextView total;
+    private Button botonAtras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,19 +48,20 @@ public class CarritoActivity extends AppCompatActivity {
         botonRealizarCompra = (Button) findViewById(R.id.botonRealizarCompraCarrito);
 
         subtotal = (TextView) findViewById(R.id.textoSubtotal);
-        String textoSubtotal = String.valueOf(Almacen.buscarPedido(s.getUsername()).calcularSubTotal());
+        String textoSubtotal = String.valueOf(Almacen.buscarPedido(s.getUsername()).getSubtotal());
         subtotal.setText(textoSubtotal);
+
         gastosEnvio = (TextView) findViewById(R.id.textoGastosEnvio);
         String textoGE = String.valueOf(Almacen.buscarPedido(s.getUsername()).getGastosDeEnvio());
         gastosEnvio.setText(textoGE);
+
         total = (TextView) findViewById(R.id.textoTotal);
         String textoTotal = String.valueOf(Almacen.buscarPedido(s.getUsername()).getTotal());
         total.setText(textoTotal);
 
-
-        Log.i("valor de usuario en carrito", s.getUsername());
-
         p = Almacen.buscarPedido(s.getUsername());
+
+        Log.i("valor de almacen al realizar el pedido", Arrays.toString(p.getItemsPedido().toArray()));
 
         for (ItemPedido i : p.getItemsPedido()) {
             LinearLayout fila = new LinearLayout(scrollView.getContext());
@@ -65,10 +72,15 @@ public class CarritoActivity extends AppCompatActivity {
 
             TextView nombre = new TextView(fila.getContext());
             nombre.setGravity(TextView.TEXT_ALIGNMENT_CENTER);
+            nombre.setGravity(TextView.TEXT_ALIGNMENT_CENTER);
+            nombre.setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.white));
+
             nombre.setText(i.getProductoPedido().getNombreProducto());
 
             TextView cantidad = new TextView(fila.getContext());
             cantidad.setGravity(TextView.TEXT_ALIGNMENT_CENTER);
+            cantidad.setGravity(TextView.TEXT_ALIGNMENT_CENTER);
+            cantidad.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.purple_500));
             String a = "x" + String.valueOf(i.getCantidadPedido());
             cantidad.setText(a);
 
