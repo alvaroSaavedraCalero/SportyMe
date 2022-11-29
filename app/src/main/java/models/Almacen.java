147ff9472b4f6fc569071e2b10d6dca106e5aba3
Alvaro Saavedra <saavedra.calero.alvaro@gmail.com>
2022-11-29 16:22:00 +0100

    Sigue sin funcionar al agregar distintos productos

--- app/src/main/java/models/Almacen.java
+++ app/src/main/java/models/Almacen.java
@@ -10,9 +10,6 @@ public class Almacen implements Serializable {
     private static ArrayList<Producto> almacenProductos = new ArrayList<>();
 
 
-
-
-
     public Almacen() {
 
 
@@ -42,57 +39,55 @@ public class Almacen implements Serializable {
                 20.99, "camiseta");
         almacenProductos.add(camiseta2);
 
-        Producto camiseta3 = new Producto("camiseta3","Camiseta Negra y Verde Runner",
+        Producto camiseta3 = new Producto("camiseta3", "Camiseta Negra y Verde Runner",
                 "Camiseta para salir a correr. Diseñada para runners profesionales",
                 24.99, "camiseta");
         almacenProductos.add(camiseta3);
 
-        Producto pantalon1 = new Producto("pantalon1","Pantalon sport Verde Adidad",
+        Producto pantalon1 = new Producto("pantalon1", "Pantalon sport Verde Adidad",
                 "Este pantalon lo puedes usar tanto para hacer deporte como para divertirte con tus amigos",
                 30.45, "pantalon");
         almacenProductos.add(pantalon1);
 
-        Producto pantalon2 = new Producto("pantalon2","Pantalon Azul Oscuro Runner",
+        Producto pantalon2 = new Producto("pantalon2", "Pantalon Azul Oscuro Runner",
                 "Pantalon diseñado para runners. Comodidad infinita",
                 40.30, "pantalon");
         almacenProductos.add(pantalon2);
 
-        Producto pantalon3 = new Producto("pantalon3","Pantalon Corto Marron Clarito",
+        Producto pantalon3 = new Producto("pantalon3", "Pantalon Corto Marron Clarito",
                 "Pantalon perfecto para el verano. Consigue el tuyo",
                 15.60, "pantalon");
         almacenProductos.add(pantalon3);
 
-        Producto sudadera1 = new Producto("sudadera1","Sudadera Negra Everlast",
+        Producto sudadera1 = new Producto("sudadera1", "Sudadera Negra Everlast",
                 "Sudadera para hombre perfecta para las epocas proximas", 30.25, "sudadera");
         almacenProductos.add(sudadera1);
 
-        Producto sudadera2 = new Producto("sudadera2","Sudadera Blanca con Imagenes",
+        Producto sudadera2 = new Producto("sudadera2", "Sudadera Blanca con Imagenes",
                 "Sudadera para la epoca navideña, puedes regalarsela a tus hijos.",
                 50.00, "sudadera");
         almacenProductos.add(sudadera2);
 
-        Producto sudadera3 = new Producto("sudadera3","Sudadera Negra Sport",
+        Producto sudadera3 = new Producto("sudadera3", "Sudadera Negra Sport",
                 "Sudadera para las personas deportistas. Forro interior",
                 43.23, "sudadera");
         almacenProductos.add(sudadera3);
 
-        Producto zapatillas1 = new Producto("zapatillas1","Zapatillas Blancas sport",
+        Producto zapatillas1 = new Producto("zapatillas1", "Zapatillas Blancas sport",
                 "Zapatillas de cuya marca no se ve perfectas para ver si no las manchas",
                 70.50, "zapatillas");
         almacenProductos.add(zapatillas1);
 
-        Producto zapatillas2 = new Producto("zapatillas2","Zapatillas basketBall",
+        Producto zapatillas2 = new Producto("zapatillas2", "Zapatillas basketBall",
                 "Zapatillas / Botas de baloncesto con camara de aire",
                 90.68, "zapatillas");
         almacenProductos.add(zapatillas2);
 
-        Producto zapatillas3 = new Producto("zapatillas3","Zapatillas negras ",
+        Producto zapatillas3 = new Producto("zapatillas3", "Zapatillas negras ",
                 "Zapatillas creo que de marca nike negras muy de calle",
                 130.20, "zapatillas");
         almacenProductos.add(zapatillas3);
     }
-
-
 
 
     public static ArrayList<User> getAlmacenUsuarios() {
@@ -112,20 +107,19 @@ public class Almacen implements Serializable {
     }
 
 
-
     public static User comprobarCredencialesLogin(String user, String pass) {
         boolean continuar = true;
         User retorno = null;
         int i = 0;
 
         while (i < almacenUsuarios.size() && continuar) {
-           if (almacenUsuarios.get(i).getUsername().equals(user) &&
-           almacenUsuarios.get(i).getPassword().equals(pass)) {
-               retorno = almacenUsuarios.get(i);
-               continuar = false;
-           } else {
-               i++;
-           }
+            if (almacenUsuarios.get(i).getUsername().equals(user) &&
+                    almacenUsuarios.get(i).getPassword().equals(pass)) {
+                retorno = almacenUsuarios.get(i);
+                continuar = false;
+            } else {
+                i++;
+            }
         }
         return retorno;
     }
@@ -156,10 +150,11 @@ public class Almacen implements Serializable {
             if (almacenUsuarios.get(i).getPedidoActual().getUsuario().equals(nombreUser)) {
                 p = almacenUsuarios.get(i).getPedidoActual();
                 continuar = false;
-            } else{
+            } else {
                 i++;
             }
-        } return p;
+        }
+        return p;
     }
 
     public static ItemPedido buscarItem(String idFoto) {
@@ -167,16 +162,19 @@ public class Almacen implements Serializable {
         int i = 0, j = 0;
         boolean continuar = true;
 
-        while (i < almacenUsuarios.size() && continuar) {
-            while (j < almacenUsuarios.get(i).getPedidoActual().getItemsPedido().size() && continuar) {
-                if (almacenUsuarios.get(i).getPedidoActual().getItemsPedido().get(j).getProductoPedido().getIdFoto().equals(idFoto)) {
-                    item = almacenUsuarios.get(i).getPedidoActual().getItemsPedido().get(j);
-                    continuar = false;
-                } else {
-                    j++;
+        while (continuar) {
+            if (i < almacenUsuarios.size()) {
+                while (j < almacenUsuarios.get(i).getPedidoActual().getItemsPedido().size() && continuar) {
+                    if (almacenUsuarios.get(i).getPedidoActual().getItemsPedido().get(j).getProductoPedido().getIdFoto().equals(idFoto)) {
+                        item = almacenUsuarios.get(i).getPedidoActual().getItemsPedido().get(j);
+                        continuar = false;
+                    } else {
+                        j++;
+                    }
                 }
+            } else {
+                i++;
             }
-            i++;
         }
         return item;
     }
